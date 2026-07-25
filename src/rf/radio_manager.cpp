594a686ed7322d78d22d427138cdfe3ad21d8003
Arxhsz <kanaantheking@gmail.com>
2026-07-25 00:16:33 -0400

    Assemble centralized V1 radio manager

---
+++ src/rf/radio_manager.cpp
@@ -0,0 +1,11 @@
+// Central V1 radio implementation is split into textual fragments to keep the
+// generated project easy to audit and update. The fragments form one C++
+// translation unit in the order included below.
+#include "radio_manager_parts/part_00.inc"
+#include "radio_manager_parts/part_01.inc"
+#include "radio_manager_parts/part_02.inc"
+#include "radio_manager_parts/part_03.inc"
+#include "radio_manager_parts/part_04.inc"
+#include "radio_manager_parts/part_05.inc"
+#include "radio_manager_parts/part_06.inc"
+#include "radio_manager_parts/part_07.inc"
